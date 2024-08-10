f8eb2ef2a71fcc37edd60085af843591ff81cbf0
Lani Kim <poikl11234@naver.com>
2024-08-10 19:36:02 +0900

    setting

---
+++ dotori/src/test/java/com/main/dotori/DotoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.dotori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotoriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

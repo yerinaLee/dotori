7e7dc284a5e36119b1428dbd1a6cfb1a5dfcbecd
Lani Kim <poikl11234@naver.com>
2024-08-22 22:04:25 +0900

    index html 수정

---
+++ dotori/src/main/java/com/main/dotori/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.main.dotori.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String mainPage(){
+        return "index";
+    }
+}
